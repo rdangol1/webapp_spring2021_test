5ec968b547020e4ddee66a9b121149235259810b
rdangol1 <dridhisha@gmail.com>
2021-02-19 10:09:29 -0700

    clasworkjs

---
+++ script.js
@@ -0,0 +1,32 @@
+function factorize(n){
+    var factors = [];
+    for(var i= 0; i <=n ; i++){
+        if (n % i ==0){
+            factors.push(i);
+        }
+    }
+    return factors;
+}
+
+function findunique(str){
+    var uniqueStr= "";
+    for (var i=0; i< str.length; i++){
+        if(uniqueStr.indexOf(str.charAt(i))==-1){
+            uniqueStr += str[i];
+        }
+    }
+    return uniqueStr;
+
+}
+
+function Dooperation(x,y,operator){
+    return operator(x,y);
+}
+
+function multiply(x, y){
+    return x*y;
+}
+
+function power(x, y){
+    return Math.pow(x,y);
+}
