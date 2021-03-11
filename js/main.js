91ecf979a3cb5cbbfabfa420063c8eb94745ccfd
rdangol1 <dridhisha@gmail.com>
2021-03-10 23:00:42 -0700

    classworkVue

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            keyword:'',
+            result:null
+        }
+    },
+    methods:{
+        searchGoogleBooks(){
+            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword +"&startIndex=0&maxResults=20")
+            .then(response => response.json())
+            .then(json => this.result = json)
+            
+            
+        }
+    }
+})
